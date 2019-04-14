1649ad9b94282ec76e5acce53d59b299ce43cf94
Allan <allanyeznaian@gmail.com>
2019-04-14 00:21:43 -0700

    asdf2

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,21 @@ import { withRouter, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Dashboard from "./Dashboard";
 import Player from "./Player";
-<<<<<<< HEAD
 import StreamList from "./StreamList";
-=======
 import Test from "./test";
->>>>>>> refs/remotes/origin/master
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <React.Fragment>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/player" component={Player} />
-        <Route exact path="/stream" component={StreamList} />
+        <div className="App">
+          <Route path="/" component={HomePage} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/player" component={Player} />
+          <Route path="/wstest" component={Test} />
+          <Route exact path="/stream" component={StreamList} />
+        </div>
       </React.Fragment>
-=======
-      <BrowserRouter>
-        <React.Fragment>
-          <div className="App">
-            <Route path="/" component={HomePage} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/player" component={Player} />
-            <Route path="/wstest" component={Test} />
-          </div>
-        </React.Fragment>
-      </BrowserRouter>
->>>>>>> refs/remotes/origin/master
     );
   }
 }
