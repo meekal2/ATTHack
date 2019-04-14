6978346295721d13c027727436195e7d803c37d6
Benjamin Lee <prizefighter@gmail.com>
2019-04-14 01:57:08 -0700

    wip

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,77 +1,69 @@
 import React from "react";
 import AudioPlayer from "react-playlist-player";
-// import Song1 from "./audio/file1";
 import "./Player.css";
+
+const $ = window.jQuery;
 
 class Player extends React.Component {
   state = {
-    isStreaming: true,
+    isStreaming: false,
+    currentSong: "",
     currentPlayList: {}
   };
-  loadPlayList = () =>
-    this.setState({
-      currentPlayList: {
-        playlistCoverUrl: "path/to/coverUrl",
-        playlistName: "playlist name",
-        bandName: "J. Cole",
-        songs: [
-          {
-            position: "1",
-            songName: "Middle Child",
-            songUrl:
-              "https://s3-us-west-1.amazonaws.com/djapp-files/09.+J.+Cole+-+MIDDLE+CHILD.mp3"
-          }
-        ],
-        type: "album"
-      }
-    });
 
   componentDidMount = () => {
-    if (this.state.isStreaming === true) {
-      this.loadPlayList();
-    }
+    var dJHubProxy = $.connection.dJHub;
+    $.connection.hub.url = "http://localhost:3001/signalr";
+    dJHubProxy.client.sendSongOne = function(one, url) {
+      this.setState({
+        isStreaming: true,
+        currentSong: one,
+        currentPlayList: {
+          songs: [
+            {
+              songUrl: url
+            }
+          ]
+        }
+      });
+    };
+
+    $.connection.hub.start().done(function() {
+      console.log("Connected!");
+    });
   };
+
   render() {
-    return (
-      <React.Fragment>
-        <div className="background">
-          {this.state.isStreaming === true ? (
-            <React.Fragment>
-              <div className={"Demo"}>
-                <button
-                  className={"Demo__load-btn"}
-                  onClick={this.loadPlayList}
-                >
-                  load playlist
-                </button>
-                <AudioPlayer
-                  currentPlayList={this.state.currentPlayList}
-                  onToggle={({ audioPlaying }) => console.log({ audioPlaying })}
-                />
+    return this.state.isStreaming === true ? (
+      <div className="playercontainer">
+        <div className="row playerimage">
+          <div className="col-12">
+            <div className="music-card playing">
+              <div className="image" />
+              <div className="wave" />
+              <div className="wave" />
+              <div className="wave" />
+              <div className="info">
+                <h2 className="title">Anomaly</h2>
+                <div className="artist">Lecrae</div>
               </div>
-              <div className="music-card-container">
-                <div className="music-card playing">
-                  <div className="image" />
-
-                  <div className="wave" />
-                  <div className="wave" />
-                  <div className="wave" />
-
-                  <div className="info">
-                    <h2 className="title">Anomaly</h2>
-                    <div className="artist">Lecrae</div>
-                  </div>
-                </div>
-
-                <i className="fa fa-pause trigger" aria-hidden="true" />
-                <i className="fa fa-play trigger" aria-hidden="true" />
-              </div>
-            </React.Fragment>
-          ) : (
-            <div>Streamer is offline!</div>
-          )}
+            </div>
+          </div>
         </div>
-      </React.Fragment>
+
+        <div className="row playercontrols">
+          <div className="col-12">
+            <div className={"Demo"}>
+              <AudioPlayer
+                currentPlayList={this.state.currentPlayList}
+                onToggle={({ audioPlaying }) => console.log({ audioPlaying })}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div>Streamer is offline!</div>
     );
   }
 }
