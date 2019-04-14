2ea7050ca8d90d3daa5f44e0393964666b9a16eb
Michael Yoo <yoo.mike@gmail.com>
2019-04-13 23:46:19 -0700

    signalR working

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router";
 import HomePage from "./HomePage";
 import Dashboard from "./Dashboard";
 import Player from "./Player";
+import Test from "./test";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
             <Route path="/" component={HomePage} />
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/player" component={Player} />
+            <Route path="/wstest" component={Test} />
           </div>
         </React.Fragment>
       </BrowserRouter>
