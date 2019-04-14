d018d211f9aee40cacbadbaaa5893a246f92660c
Benjamin Lee <prizefighter@gmail.com>
2019-04-13 20:14:42 -0700

    wip

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,8 +1,30 @@
 import React from "react";
+import './Player.css'; 
 
 class Player extends React.Component {
   render() {
-    return <div />;
+    return (
+   
+        <div class='music-card playing'>
+  
+        <div class='image'></div>
+        
+        <div class='wave'></div>
+        <div class='wave'></div>
+        <div class='wave'></div>
+        
+        <div class='info'>
+          <h2 class='title'>Anomaly</h2>
+          <div class='artist'>Lecrae</div>
+        </div>
+        
+        <i class="fa fa-pause trigger" aria-hidden="true"></i>
+        <i class="fa fa-play trigger" aria-hidden="true"></i>
+      </div>
+      
+      
+
+        );
   }
 }
 
